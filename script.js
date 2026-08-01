03f6b4146a667d6e0da8fe9a9d18b9e0b5fa169a
mayaelashri <mayaelashri@gmail.com>
2026-08-01 22:19:30 +0300

    first upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeText() {
+  document.getElementById("title").innerText = "Enjoy your coffee 🤎";
+  document.getElementById("text").innerText = "Text changed successfully ☕";
+}
+
+function changeLayout() {
+  let box = document.getElementById("boxes");
+
+  if (box.style.flexDirection === "column") {
+    box.style.flexDirection = "row";
+  } else {
+    box.style.flexDirection = "column";
+  }
+}
